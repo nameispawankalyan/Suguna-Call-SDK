c05a6f2c9b9ea600e33c31c815adb60e63345f70
nameispawankalyan <pawankalyan4241@gmail.com>
2026-02-02 06:57:43 +0530

    Fixed SugunaVideoView syntax

--- android-sdk/suguna-rtc/src/main/java/com/suguna/rtc/SugunaVideoView.kt
+++ android-sdk/suguna-rtc/src/main/java/com/suguna/rtc/SugunaVideoView.kt
@@ -8,9 +8,8 @@ import io.livekit.android.renderer.TextureViewRenderer
  * SugunaVideoView - A wrapper around the underlying video renderer.
  * This ensures the consumer app doesn't need to know about LiveKit specifically.
  */
-class SugunaVideoView @JvmOverloads constructor(
-    context: Context, 
-    attrs: AttributeSet? = null
-) : TextureViewRenderer(context, attrs) {
-    // We can add custom initialization or styling here if needed in future.
+class SugunaVideoView : TextureViewRenderer {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 }
+
